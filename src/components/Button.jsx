0993de2ba6bca2f62f710347b11cfad16ffd2f74
LilPessy <pettidaniele1f@gmail.com>
2023-03-15 16:40:36 +0100

    creato il componente Button
    componente con relativo stile (da rivedere lo stile e da fare la logica)

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,21 @@
+import { useState } from 'react'
 import '../styles/Button.css'
 
-function Button() {
+function Button({clbk, variant, msg}) {
+
+  function renderClasses(){
+    let classes = ["btn", "d-flex", "bgRed", variant];
+    return classes.join(" ")
+  }
+
+  function componentClickHandler(){
+    //condizioni
+    clbk()
+  }
+
   return (
-    <div className="btn">
-        
+    <div className={renderClasses()} onClick={()=>{componentClickHandler()}}>
+        {msg}
     </div>
   )
 }
