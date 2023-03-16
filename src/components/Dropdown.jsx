0c94a49887b2e42235ae2511dbbbd276d1c1e609
LilPessy <pettidaniele1f@gmail.com>
2023-03-16 22:29:08 +0100

    aggiornata dropdown
    probabilmente finito (problema sulla clbk)

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,19 +1,20 @@
 import "../styles/Dropdown.css"
+import { useEffect } from "react";
 
-function Dropdown({variant, list, clbk}){
+function Dropdown({list, clbk}){
     function renderClasses(){
         let classes = ["dropdown", "flexCol"];
         return classes.join(" ")
-      }
+    }
     
     function handleChange(e){
-        clbk(e.target.value)
+        clbk(e.currentTarget.value)
     }  
 
     return(
         <div className={renderClasses()}>
             <select onChange={(e)=>{handleChange(e)}}>
-                <option value = "">Scegli una categoria</option>
+                <option>Scegli una categoria</option>
                 {list.map((item, index)=>{
                    return(<option key={index} value={item} >{item.charAt(0).toUpperCase() + item.slice(1)}</option>)
                 })}
